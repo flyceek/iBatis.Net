751665ff33dc4adc0536098b8684d8b6d548306e
paranora <yangqun@centaline.com.cn>
2023-04-06 15:10:43 +0800

    u

--- IBatisNet.Common/Properties/AssemblyInfo.cs
+++ IBatisNet.Common/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyDescription("Common object used by DataAccess and DataMapper component in iBATIS.Net")]
 [assembly: AssemblyTrademark("Licensed under the Apache License, Version 2.0")]
 [assembly: AssemblyCopyright("Copyright 2007,2005 The Apache Software Foundation")]
-[assembly: AssemblyProduct("iBATIS.NET")]
+[assembly: AssemblyProduct("iBATIS.NET Of Paranora")]
 [assembly: CompilationRelaxations(8)]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
